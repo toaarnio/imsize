cac11d466626ca21be03d32efe3c2b5fc916e33b
Tomi Aarnio <tomi.p.aarnio@gmail.com>
2023-07-27 09:47:34 +0300

    [doc] Update package description in __init__.py
    This commit constitutes release 1.3.5.

--- imsize/__init__.py
+++ imsize/__init__.py
@@ -1,5 +1,14 @@
 """
-Foobar -- this __init__.py docstring shouldn't be visible anywhere.
+Extracts image dimensions, bit depth, and other basic metadata.
+Runs very fast due to reading and parsing only the file header,
+with the exception of proprietary camera RAW files.
+
+Example:
+  info = imsize.read("myfile.jpg")
+  factor = info.nbytes / info.filesize
+  print(f"{info.filespec}: compression factor = {factor.1f}")
+
+https://github.com/toaarnio/imsize
 """
 
 from .imsize import read, ImageInfo
